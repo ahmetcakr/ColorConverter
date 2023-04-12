193c40ba8255a516bacee24cf2aec92c159a6008
ahmetcakr <ahmet.cakir@yalin.com.tr>
2023-04-12 17:11:29 +0300

    added

---
+++ ColorConverter/DelphiColors.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace ColorConverter
+{
+    public static class DelphiColors
+    {
+        private static Dictionary<string, Color> colorDictionary = new Dictionary<string, Color>() {
+        { "clWhite", Color.White },
+        { "clBlack", Color.Black },
+        { "clRed", Color.Red },
+        };
+
+        public static Color Convert(string renkKodu)
+        {
+            if (colorDictionary.ContainsKey(renkKodu))
+            {
+                return colorDictionary[renkKodu];
+            }
+            else
+            {
+                throw new ArgumentException("Geçersiz renk kodu");
+            }
+        }
+    }
+}
